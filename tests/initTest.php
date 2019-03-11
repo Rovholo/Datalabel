81d5ec8774032991a2710852d9452bf54c45a297
Rovholo <1432447@students.wits.ac.za>
2019-03-11 19:14:38 +0200

    Update initTest.php

--- tests/initTest.php
+++ tests/initTest.php
@@ -2,7 +2,7 @@
 /**
  * @coversDefaultClass \Rovholo\datalabel\init
  */
-class initTest extends \PHPUnit_Framework_TestCase{
+class initTest extends PHPUnit_Framework_TestCase{
   protected $hello;
   
   public function setUp(){//this part of the code initiates the hello variable
