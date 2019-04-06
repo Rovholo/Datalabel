b26231ab6529c948dfccbc6d18978af3944acf72
LennyBokgoshi <30719310+LennyBokgoshi@users.noreply.github.com>
2019-04-06 15:54:31 +0200

    Add files via upload

---
+++ tests/initTest2.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @coversDefaultClass \Rovholo\datalabel\init
+ */
+class initTest extends PHPUnit_Framework_TestCase{
+  protected $myname;
+
+  public function setUp(){//this part of the code initiates the hello variable
+    $this->$myname = new \Rovholo\datalabel\init();
+  }
+  /**
+   * @covers ::world
+   */
+  public function testinit(){//this part of the code checks if the value returned by the world() method is equal to word
+    $this->assertSame('name',$this->myname->name());
+  }
+
+}
+?>
