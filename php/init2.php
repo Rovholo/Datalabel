6009324bf941e983feb02065908819b9da80beb9
LennyBokgoshi <30719310+LennyBokgoshi@users.noreply.github.com>
2019-04-08 11:45:10 +0200

    Update init2.php

--- php/init2.php
+++ php/init2.php
@@ -1,8 +1,20 @@
 <?php
 namespace Rovholo\datalabel;
 class init2{
-  public function name(){
-    return 'name';
+  public function connectDB(){
+    $servername = "localhost";
+    $username = "piykvshj";
+    $password = "Differ123*cpanel";
+
+  // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+  // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
   }
+   
 }
 ?>
