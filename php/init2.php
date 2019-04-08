1166656a0755b709aa5cddcbd06e315e25762010
Rovholo <1432447@students.wits.ac.za>
2019-04-08 12:01:09 +0200

    Update init2.php

--- php/init2.php
+++ php/init2.php
@@ -13,7 +13,7 @@ class init2{
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    return "Connected successfully";
   }
    
 }
