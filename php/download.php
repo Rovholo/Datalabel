51024e447ca05bd8e65b6fbcfd98a6c0d1a3eca2
Rovholo <1432447@students.wits.ac.za>
2019-04-19 01:00:49 +0200

    Update download.php

--- php/download.php
+++ php/download.php
@@ -1,24 +1,28 @@
 <?php
-    $servername = "localhost";
-    $username = "piykvshj";
-    $password = "Differ123*cpanel";
-    
-    $link = new mysqli($servername,$username,$password,"piykvshj_database1");
-
-    if ($link->connect_error) {
-        die("connection failed: " . $link->connect_error);
+    namespace Rovholo\datalabel;
+    class download{
+        function connectDetail() {
+            $servername = "localhost";
+            $username = "piykvshj";
+            $password = "Differ123*cpanel";
+            
+            $link = new mysqli($servername,$username,$password,"piykvshj_database1");
+            
+            if ($link->connect_error) {
+                die("connection failed: " . $link->connect_error);
+            }
+            
+            $result = mysqli_query($link,"SELECT * FROM images2");
+            
+            $link->close();
+            
+            $arr = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                array_push($arr, $row);
+            }
+            echo json_encode($arr);
+            
+            return $result;
+        }
     }
-    $user = $_POST['Uname'];
-    #echo "now". $user. "thats ric";
-    $result = mysqli_query($link,"SELECT * FROM images2");
-    $link->close();
-    #echo "connected successfully <br />";
-    #echo json_encode($result);
-    $arr = array();
-    while ($row = mysqli_fetch_assoc($result)) 
-    {
-        #echo json_encode($result);
-        array_push($arr, $row);
-    }
-    echo json_encode($arr);
 ?>
