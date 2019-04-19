47df470e517bcc3f26563119d61002c2ce5b9d25
Rovholo <1432447@students.wits.ac.za>
2019-04-19 02:53:34 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -2,14 +2,7 @@
     namespace Rovholo\datalabel;
     class connect{
         function connectDetail() {
-            $username = "user";
-            $password = "password";
-            $database = "database1";
-            $link = mysqli_connect("127.0.0.1", $username ,$password , $database);
-            return $link;
-        }
-        function closeConnection(){
-            mysqli_close($this->connectDetail());
+            return mysqli_connect("127.0.0.1", "user" ,"password" , "database1");
         }
     }
 ?>
