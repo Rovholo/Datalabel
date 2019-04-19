85df7ee0d85f0c309236671c3a7175011e268dfe
Rovholo <1432447@students.wits.ac.za>
2019-04-19 02:02:10 +0200

    Update downloadTest.php

--- tests/downloadTest.php
+++ tests/downloadTest.php
@@ -12,6 +12,9 @@ class downloadTest extends PHPUnit_Framework_TestCase{
    * @covers ::request
    */
   public function testinit(){//this part of the code checks if the value returned by the world() method is equal to word
+    $link = mysqli_connect("localhost","user","password","database1");
+    $result = mysqli_query($link,"CREATE TABLE images2 (img_id int,img_name varchar(255),img_data varchar(255));");
+    
     $this->assertFalse($this->result->request());
   }
 }
