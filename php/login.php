5938d158e28b4310a4c36ddf012743b334f07031
Rovholo <1432447@students.wits.ac.za>
2019-05-29 22:21:39 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -2,17 +2,19 @@
     namespace Rovholo\datalabel;
     class login{
         function login($username) {
-        $link = new mysqli("localhost","piykvshj","Differ123*cpanel","piykvshj_database1");
-        if ($link->connect_error) {
-            die("connection failed: " . $link->connect_error);
+            $link = new mysqli("localhost","piykvshj","Differ123*cpanel","piykvshj_database1");
+            if ($link->connect_error) {
+                die("connection failed: " . $link->connect_error);
+            }
+            #$user = $_POST['Uname'];
+            $result = mysqli_query($link,"SELECT * FROM users where user_name='$username'");
+            $link->close();
+            $arr = array();
+            while ($row = mysqli_fetch_assoc($result)) 
+            {
+                array_push($arr, $row);
+            }
+                return json_encode($arr);
         }
-        #$user = $_POST['Uname'];
-        $result = mysqli_query($link,"SELECT * FROM users where user_name='$username'");
-        $link->close();
-        $arr = array();
-        while ($row = mysqli_fetch_assoc($result)) 
-        {
-            array_push($arr, $row);
-        }
-        return json_encode($arr);
+    }
 ?>
