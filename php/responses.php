5807e243c76466f0dc3577ebe88749ffa4ff01fa
Rovholo <1432447@students.wits.ac.za>
2019-05-30 15:23:45 +0200

    Update responses.php

--- php/responses.php
+++ php/responses.php
@@ -1,6 +1,6 @@
 <?php
     namespace Rovholo\datalabel;
-    class response{
+    class responses{
         function response() {
             $link = mysqli_connect("localhost","user","password","database1");
             if ($link->connect_error) {
