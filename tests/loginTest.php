4ead0680d07d4b8f36025baee9794743a80448b4
Rovholo <1432447@students.wits.ac.za>
2019-05-30 02:38:13 +0200

    Update loginTest.php

--- tests/loginTest.php
+++ tests/loginTest.php
@@ -7,6 +7,8 @@ class loginTest extends PHPUnit_Framework_TestCase{
   
   public function setUp(){//this part of the code initiates the hello variable
     $this->result = new \Rovholo\datalabel\login();
+    $link = mysqli_connect("localhost","user","password","database1");
+    mysqli_query($link,"CREATE TABLE users (user_id int,user_name varchar(255),password varchar(255));");
   }
   /**
    * @covers ::login
@@ -14,7 +16,7 @@ class loginTest extends PHPUnit_Framework_TestCase{
   public function testinit(){//this part of the code checks if the value returned by the world() method is equal to word
     $result = $this->result->login("user");
     $password = "name";
-    $this->assertTrue(json_decode($result) != "error" || json_decode($result)[0]->password == $password);
+    $this->assertTrue(json_decode($result)[0]->password == $password);
   }
   
   public function testinit2(){//this part of the code checks if the value returned by the world() method is equal to word
