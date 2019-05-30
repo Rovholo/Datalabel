bc6129ffd88fa75953f047e63ced38f7444722ed
Rovholo <1432447@students.wits.ac.za>
2019-05-30 10:28:25 +0200

    Update outputTest.php

--- tests/outputTest.php
+++ tests/outputTest.php
@@ -6,7 +6,7 @@ class outputTest extends PHPUnit_Framework_TestCase{
   protected $result;
   
   public function setUp(){//this part of the code initiates the hello variable
-    $this->result = new \Rovholo\datalabel\upload();
+    $this->result = new \Rovholo\datalabel\output();
     $link = mysqli_connect("localhost","user","password","database1");
     mysqli_query($link,"CREATE TABLE images2 (img_id int,img_name varchar(255),img_data varchar(255));");
   }
