13d91bdfd21bb5482d43b61beeb436f1fa21c9e8
Rovholo <1432447@students.wits.ac.za>
2019-05-30 14:06:51 +0200

    Update labeling.php

--- php/labeling.php
+++ php/labeling.php
@@ -1,23 +1,23 @@
 <?php
-    $servername = "localhost";
-    $username = "piykvshj";
-    $password = "Differ123*cpanel";
+    namespace Rovholo\datalabel;
+    class labelling{
+        function labels($user) {
+            $link = mysqli_connect("localhost","user","password","database1");
+            if ($link->connect_error) {
+                die("connection failed: " . $link->connect_error);
+            }
     
-    $link = new mysqli($servername,$username,$password,"piykvshj_database1");
-
-    if ($link->connect_error) {
-        die("connection failed: " . $link->connect_error);
+            $result = mysqli_query($link,"SELECT * FROM labels");
+    
+            $link->close();
+    
+            $arr = array();
+            while ($row = mysqli_fetch_assoc($result)) 
+            {
+                array_push($arr, $row);
+            }
+            echo json_encode($arr);
+            return json_encode($arr);
+        }
     }
-    
-    $result = mysqli_query($link,"SELECT * FROM labels");
-    
-    $link->close();
-    
-    $arr = array();
-    while ($row = mysqli_fetch_assoc($result)) 
-    {
-        #echo json_encode($result);
-        array_push($arr, $row);
-    }
-    echo json_encode($arr);
 ?>
