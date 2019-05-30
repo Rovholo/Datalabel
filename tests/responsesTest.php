7251d872929a27040bcf7eb303a187c6148d78ad
Rovholo <1432447@students.wits.ac.za>
2019-05-30 15:24:22 +0200

    Update and rename responseTest.php to responsesTest.php

--- tests/responsesTest.php
+++ tests/responsesTest.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @coversDefaultClass \Rovholo\datalabel\response
+ * @coversDefaultClass \Rovholo\datalabel\responses
  */
-class responseTest extends PHPUnit_Framework_TestCase{
+class responsesTest extends PHPUnit_Framework_TestCase{
   protected $result;
   
   public function setUp(){//this part of the code initiates the hello variable
-    $this->result = new \Rovholo\datalabel\response();
+    $this->result = new \Rovholo\datalabel\responses();
     $link = mysqli_connect("localhost","user","password","database1");
     mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),password varchar(255),user_role varchar(255),user_rating varchar(255))");
     mysqli_query($link,"INSERT INTO users (user_id,user_name,password,user_role,user_rating) values('1','user','name','manager','0')");
