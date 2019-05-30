d1b140579f37cfcc0b220496610b5d7b5fa270df
Rovholo <1432447@students.wits.ac.za>
2019-05-30 10:00:19 +0200

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,27 +1,21 @@
 <?php
-    $servername = "localhost";
-    $username = "piykvshj";
-    $password = "Differ123*cpanel";
+     namespace Rovholo\datalabel;
+    class register{
+        function register($username,$email,$pass) {
+            $link = mysqli_connect("localhost","user","password","database1");
+            if ($link->connect_error) {
+                die("connection failed: " . $link->connect_error);
+            }
+            if($username == "" || $pass == "" || $email == "")  {
+                return "invalid input";
+            }
+            if( !( strpos($email,"@") && strpos($email, ".") && count($email) > 10 ) ) {
+                return "invalid mail";
+            }
     
-    $link = new mysqli($servername,$username,$password,"piykvshj_database1");
-
-    if ($link->connect_error) {
-        die("connection failed: " . $link->connect_error);
+            $result = mysqli_query( $link,"INSERT INTO users(user_name, user_email, user_role, password, user_rating) VALUES('$user', '$email', 'master', '$pass','0')" );
+            $link->close();
+            return $result;
+        }
     }
-    $user = $_POST['Uname'];
-    $mail = $_POST['Umail'];
-    $pass = $_POST['passwd'];
-    
-    #echo "now". $user. "thats ric";
-    $result = mysqli_query( $link,"INSERT INTO users(user_name, user_email, user_role, password, user_rating) VALUES('$user', '$mail', 'master', '$pass','0')" );
-    $link->close();
-    #echo "connected successfully <br />";
-    #echo json_encode($result);
-    $arr = array();
-    while ($row = mysqli_fetch_assoc($result)) 
-    {
-        #echo json_encode($result);
-        array_push($arr, $row);
-    }
-    echo json_encode($arr);
 ?>
