adc08d51b9ff9866cdc3434022de903d7034de65
Rovholo <1432447@students.wits.ac.za>
2019-05-30 15:28:39 +0200

    Update responses.php

--- php/responses.php
+++ php/responses.php
@@ -7,15 +7,16 @@
                 die("connection failed: " . $link->connect_error);
             }
             $result = mysqli_query($link,"SELECT * FROM users where user_role != 'manager'");
-            $arr = array();
-            if ($result->num_rows > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    array_push($arr, $row);
-                }
-            } else {
-                array_push($arr, "0 results");
-            }
-            echo json_encode($arr);
+            #$arr = array();
+            #if ($result->num_rows > 0) {
+                #while ($row = mysqli_fetch_assoc($result)) {
+                    #array_push($arr, $row);
+                #}
+            #} else {
+                #array_push($arr, "0 results");
+            #}
+            #echo json_encode($arr);
+            return $result;
         }
     }
 
