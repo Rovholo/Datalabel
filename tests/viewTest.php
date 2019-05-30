2335e5dd22d7ed9ad4685abe2412631fb60d4943
Rovholo <1432447@students.wits.ac.za>
2019-05-30 13:56:14 +0200

    Update viewTest.php

--- tests/viewTest.php
+++ tests/viewTest.php
@@ -10,12 +10,13 @@ class viewTest extends PHPUnit_Framework_TestCase{
     $this->result = new \Rovholo\datalabel\view();
     $link = mysqli_connect("localhost","user","password","database1");
     mysqli_query($link,"CREATE TABLE label_img ( user_id int,img_id int,img_name varchar(255),img_type varchar(255),img_data varchar(255),img_labels varchar(255) )");
+    mysqli_query($link,"INSERT INTO label_img (user_id, img_id, img_name, img_type, img_data,img_labels) VALUES ('1','input','input', 'input', 'input','input')");
   }
   /**
    * @covers ::download
    */
   public function testinit(){
-    $this->assertTrue($this->result->download() != false);
+    $this->assertTrue($this->result->download(1) != false);
   }
 }
 ?>
