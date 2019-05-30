a6041c9ce6c55616d12affa00ae01bc2756cbad7
Rovholo <1432447@students.wits.ac.za>
2019-05-30 13:47:56 +0200

    Update view.php

--- php/view.php
+++ php/view.php
@@ -1,27 +1,26 @@
 <?php
-    $servername = "localhost";
-    $username = "piykvshj";
-    $password = "Differ123*cpanel";
+    namespace Rovholo\datalabel;
+    class view{
+        function download($username) {
+            $link = mysqli_connect("localhost","user","password","database1");
+            if ($link->connect_error) {
+                die("connection failed: " . $link->connect_error);
+            }
     
-    $user = $_POST['userID'];
+            #$user = $_POST['userID'];
+            $result = mysqli_query($link,"SELECT * FROM label_img WHERE user_id='$user'");
     
-    $link = new mysqli($servername,$username,$password,"piykvshj_database1");
-
-    if ($link->connect_error) {
-        die("connection failed: " . $link->connect_error);
+            $link->close();
+            #echo "connected successfully <br />";
+            #echo json_encode($result);
+            $arr = array();
+            while ($row = mysqli_fetch_assoc($result)) 
+            {
+                #echo json_encode($result);
+                array_push($arr, $row);
+            }
+            echo json_encode($arr);
+        }
     }
-    
-    $result = mysqli_query($link,"SELECT * FROM label_img WHERE user_id='$user'");
-    
-    $link->close();
-    #echo "connected successfully <br />";
-    #echo json_encode($result);
-    $arr = array();
-    while ($row = mysqli_fetch_assoc($result)) 
-    {
-        #echo json_encode($result);
-        array_push($arr, $row);
-    }
-    echo json_encode($arr);
 
 ?>
