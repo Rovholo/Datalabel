5432253093768ba202f2f046e20ee607707ac4bd
Rovholo <1432447@students.wits.ac.za>
2019-05-30 15:07:07 +0200

    Update responses.php

--- php/responses.php
+++ php/responses.php
@@ -1,25 +1,22 @@
 <?php
-    $servername = "localhost";
-    $username = "piykvshj";
-    $password = "Differ123*cpanel";
-
-
-    $link = new mysqli($servername,$username,$password,"piykvshj_database1");
-    if ($link->connect_error) {
-        die("connection failed: " . $link->connect_error);
-    }
-
-    $result = mysqli_query($link,"SELECT * FROM users where user_role != 'manager'");
-    $arr = array();
-    if ($result->num_rows > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            array_push($arr, $row);
+    namespace Rovholo\datalabel;
+    class response{
+        function response() {
+            $link = mysqli_connect("localhost","user","password","database1");
+            if ($link->connect_error) {
+                die("connection failed: " . $link->connect_error);
+            }
+            $result = mysqli_query($link,"SELECT * FROM users where user_role != 'manager'");
+            $arr = array();
+            if ($result->num_rows > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($arr, $row);
+                }
+            } else {
+                array_push($arr, "0 results");
+            }
+            echo json_encode($arr);
         }
-    } else {
-        array_push($arr, "0 results");
     }
-    echo json_encode($arr);
-
-$conn->close();
 
 ?>
