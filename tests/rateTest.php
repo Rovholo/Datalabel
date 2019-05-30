34266f9398b85971f286f6a2c5848f017307a8e5
Rovholo <1432447@students.wits.ac.za>
2019-05-30 15:35:31 +0200

    Update rateTest.php

--- tests/rateTest.php
+++ tests/rateTest.php
@@ -8,8 +8,8 @@ class rateTest extends PHPUnit_Framework_TestCase{
   public function setUp(){//this part of the code initiates the hello variable
     $this->result = new \Rovholo\datalabel\rate();
     $link = mysqli_connect("localhost","user","password","database1");
-    mysqli_query($link,"CREATE TABLE users (user_id int,user_name varchar(255),password varchar(255),user_rating int)");
-    mysqli_query($link,"INSERT INTO users (user_id,user_name,password,user_rating) values('1','user','name','0')");
+    mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),password varchar(255),user_role varchar(255),user_rating varchar(255))");
+    mysqli_query($link,"INSERT INTO users (user_id,user_name,password,user_role,user_rating) values('1','user','name','manager','0')");
   }
   /**
    * @covers ::rate
