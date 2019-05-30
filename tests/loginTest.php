37371e9eddb28a52d806a4b40a766e9ae4033f1f
Rovholo <1432447@students.wits.ac.za>
2019-05-30 03:07:52 +0200

    Update loginTest.php

--- tests/loginTest.php
+++ tests/loginTest.php
@@ -8,8 +8,8 @@ class loginTest extends PHPUnit_Framework_TestCase{
   public function setUp(){//this part of the code initiates the hello variable
     $this->result = new \Rovholo\datalabel\login();
     $link = mysqli_connect("localhost","user","password","database1");
-    mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),password varchar(255));");
-    mysqli_query($link,"INSERT INTO users (user_name,password) values('user','name');");
+    mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),password varchar(255))");
+    mysqli_query($link,"INSERT INTO users (user_id,user_name,password) values('1','user','name')");
   }
   /**
    * @covers ::login
